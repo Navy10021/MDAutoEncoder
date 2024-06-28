a81d102f2de2585beb8abcbfe7fdf430a4871a91
Yoonseop Lee <navy10021@snu.ac.kr>
2024-06-28 22:04:38 +0900

    Add files via upload

---
+++ code/help_f.py
@@ -0,0 +1,59 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Reconstruction error : mean + 3 * standard deviation
+def compute_reconstruction_error(data, model):
+    model.eval() # using trained model
+    with torch.no_grad():
+        data = data.to(device)
+        output = model(data)
+        recon_error = torch.mean((data - output) ** 2, dim=[1, 2, 3])
+    return recon_error.cpu().detach().numpy()
+
+
+def plot_reconstructed_images(model, data):
+    model.eval()
+    with torch.no_grad():
+        data = data.to(device)
+        output = model(data)
+
+    # Visualization of original and reconstructed images (recovered by the model)
+    fig, axes = plt.subplots(2, 10, figsize=(15, 3))
+    for i in range(10):
+        axes[0, i].imshow(data[i].cpu().numpy().squeeze(), cmap='gray')
+        axes[0, i].axis('off')
+        axes[0, i].set_title('Original', fontsize=8, pad=2)
+
+        axes[1, i].imshow(output[i].cpu().numpy().squeeze(), cmap='gray')
+        axes[1, i].axis('off')
+        axes[1, i].set_title('Reconstructed', fontsize=8, pad=2)
+
+    plt.suptitle('Original and Reconstructed Malware Images', fontsize=13, y=1.15)
+    plt.show()
+    
+
+def plot_latent_space(data, model):
+    model.eval()
+    with torch.no_grad():
+        data = data.to(device)
+        latent = model.encoder(data)  # Assuming the encoder part of the autoencoder is accessible
+        latent = latent.view(latent.size(0), -1).cpu().numpy()
+
+    tsne = TSNE(n_components=2)
+    latent_2d = tsne.fit_transform(latent)
+
+    plt.figure(figsize=(10, 6))
+    plt.scatter(latent_2d[:, 0], latent_2d[:, 1], c='blue', alpha=0.5)
+    plt.xlabel('TSNE Component 1')
+    plt.ylabel('TSNE Component 2')
+    plt.title('Latent Space Visualization using TSNE')
+    plt.grid(True)
+    plt.show()
