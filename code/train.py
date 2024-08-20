8cb6dfe302802a3cb4a3e119523f24f05b8609b2
Yoon-seop Lee <navy10021@snu.ac.kr>
2024-08-20 17:46:24 +0900

    train.py 업데이트

---
+++ code/train.py
@@ -0,0 +1,42 @@
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(">> My model using", device)
+
+# model 1 : CNN based model
+#model = CNNAutoencoder().to(device)
+# model 2 : ResNet based model
+model = ResNetAutoencoder().to(device)
+criterion = nn.MSELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+num_epochs = 30
+
+# Training
+train_losses = []
+for epoch in range(num_epochs):
+    epoch_loss = 0
+
+    with tqdm(total=len(train_loader), desc=f'Epoch {epoch + 1}/{num_epochs}', unit='batch') as pbar:
+        for data, _ in train_loader:
+            data = data.to(device)
+            optimizer.zero_grad()
+            output = model(data)
+            loss = criterion(output, data)
+            loss.backward()
+            optimizer.step()
+            epoch_loss += loss.item()
+            pbar.set_postfix(loss=loss.item())
+            pbar.update(1)
+
+    train_losses.append(epoch_loss / len(train_loader))
+
+# Plotting the training loss curve
+plt.figure(figsize=(10, 6))
+plt.plot(range(1, num_epochs + 1), train_losses, label='Training Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.title('Training Loss Curve')
+plt.legend()
+plt.grid(True)
+plt.show()
