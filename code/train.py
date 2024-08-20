1e71d36f20a664512dab9b3492635cf9347b2044
Yoon-seop Lee <navy10021@snu.ac.kr>
2024-08-20 17:51:20 +0900

    train.py 업데이트

--- code/train.py
+++ code/train.py
@@ -1,3 +1,11 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms, utils
+from torch.utils.data import DataLoader
+
+
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
